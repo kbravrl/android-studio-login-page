258a6fab614492fe667829fbbd8cefc9c4dd19cb
Kübra Varol <115376553+kbravrl@users.noreply.github.com>
2024-10-11 14:34:55 +0300

    Add files via upload

---
+++ app/src/main/java/com/example/loginpageinandroidstudio/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.loginpageinandroidstudio;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText username;
+    EditText password;
+    Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(username.getText().toString().equals("user") && password.getText().toString().equals("1234")){
+                    Toast.makeText(MainActivity.this, "Login Succesfully", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
+    }
+}
